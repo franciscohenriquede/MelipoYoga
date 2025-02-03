7dbd35f325148c12bcb3c90f6abf0b19c7f4690f
francisco Moura <franciscohenriquede@gmail.com>
2025-02-03 07:13:39 -0300

    Mensagem do commit

--- js/Meditacao.js
+++ js/Meditacao.js
@@ -1,35 +1,37 @@
- 
- let textos = [
-    "Enquanto estiver expandindo inspire devagar,preenchendo toda barriga, todo o peito e do pulmão!",
-    "Agora exale profundamente, eliminando todo o ar.",
-    "Repita a respiração profundamente, mantendo a atenção no momento presente."
-  ];
-  let index = -1;
-  let interval;
 
-  function alterarTitulo() {
-    const botao = document.querySelector('a[onclick="alterarTitulo()"]');
+let textos = [
+"Enquanto estiver expandindo inspire devagar,preenchendo toda barriga, todo o peito e do pulmão!",
+"Agora exale profundamente, eliminando todo o ar.",
+"Repita a respiração profundamente, mantendo a atenção no momento presente."
+];
 
-    // Verifica se o elemento e o botão existem
+let index = 0;
 
-    if (botao) {
-      botao.innerHTML = ""; // Altera o texto do botão
-    }
-
-    // Começa a sequência de mudanças automáticas
-    if (!interval) {
-      interval = setInterval(() => {
-        index = (index + 1) % textos.length; // Alterna entre os textos do array
-        const textoprincipal = document.getElementById('textoprincipal');
-
-        // Oculta o texto gradualmente
-        textoprincipal.style.opacity = '0';
-
-        // Aguarda a transição de ocultar antes de alterar o texto
-        setTimeout(() => {
-          textoprincipal.innerHTML = textos[index]; // Usa innerHTML para suportar formatação
-          textoprincipal.style.opacity = '0.8'; // Mostra o texto novamente
-        }, 1000); // Tempo para a transição
-      }, 4000); // Intervalo de 4 segundos entre as mensagens
-    }
+function alterarTitulo() {
+  const botao = document.querySelector('a[onclick="alterarTitulo()"]');
+  if (botao) {
+      botao.innerHTML = "";
   }
+
+  const textoprincipal = document.getElementById('textoprincipal');
+  const contador = document.getElementById('contador');
+  if (!textoprincipal || !contador) return;
+
+ 
+
+  function mostrarProximoTexto() {
+      if (index < textos.length) {
+          textoprincipal.style.opacity = '0';
+          setTimeout(() => {
+              textoprincipal.innerHTML = textos[index];
+              textoprincipal.style.opacity = '0.8';
+              index++;
+              if (index < textos.length) {
+                  iniciarContador(4, mostrarProximoTexto);
+              }
+          }, 1000);
+      }
+  }
+
+  mostrarProximoTexto();
+}
